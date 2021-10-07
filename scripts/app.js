6fa3e19b5bb9c2327c2c868b53014a7ffbb12b55
lukacspapp <papplukacs@hotmail.com>
2021-10-08 00:03:22 +0100

    increased Layout

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,15 @@ function init() {
 
   // -----Grid
   const grid = document.querySelector('.grid')
-  const width = 15
-  const cellCount = width * width
+  const width = 12
+  const height = 22.7
+  const cellCount = width * height
   const cells = []
+  // ----- sound
+  const backgroundMusic = document.querySelector('audio')
+
   //-----Player
-  const playerStartingPosition = 217
+  const playerStartingPosition = 262
   let playerCurrentPosition = 0
   const playerClass = 'player'
   let livesScore = document.querySelector('span')
@@ -18,11 +22,13 @@ function init() {
   const alienStartingPosition = 0
   let alienCurrentPosition = 0  
   const aliens = [
-    2,3,4,5,6,8,9,10,11,12,
-    16,17,18,19,20,21,23,24,25,26,27,28,
-    31,32,33,34,35,36,38,39,40,41,42,43,
-    47,48,49,50,51,53,54,55,56,57
+    4,5,6,7,8,9,11,12,13,14,15,16
+    ,24,25,26,27,28,29,30,32,33,34,35,36,37,38
+    ,45,46,47,48,49,50,51,53,54,55,56,57,58,59,
+    67,68,69,70,71,72,74,75,76,77,78,79
+    
   ]
+
 
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
